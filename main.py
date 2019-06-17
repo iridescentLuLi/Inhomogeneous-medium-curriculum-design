40f7b2cb02924b657a341f2a66fb5479cdf3a1c5
liushuang <517085537@qq.com>
2019-06-17 12:04:31 +0800

    final release

--- main.py
+++ main.py
@@ -1,34 +1,28 @@
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 import matplotlib.pyplot as plt
-from PyQt5 import QtCore
-from Qt import QtGui
 
 from main_window import Ui_MainWindow
 import numpy as np
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from threading import Thread
-import time
 import re
 
 
 frequency = 0
 level_number = 0
 medium_original = []
-type_of_wave = 0
-#1 for TM , 2 for TE , 0 for none
 figure_plot = []
 yita = []
 miu = []
 ebuxiro = []
 d_each_level = []
-R_TE = []
-R_TM = []
-T_TE = []
-T_TM = []
-wave_Mode = 0  #1 for TE, 2 for TM
-curve_Mode = 0  #1 for reflection， 2 for transmission
-
+wave_Mode = 0  #1 for TE, 2 for TM, 0 for none
+curve_Mode = 0  #1 for reflection， 2 for transmission, 0 for none
+R_TE_current = 0
+R_TM_current = 0
+T_TE_current = 0
+T_TM_current = 0
+Z_n1 = 0
 
 class AppWindow(QMainWindow):
     def __init__(self):
@@ -78,16 +72,58 @@ class AppWindow(QMainWindow):
             return
 
         for i in range(level_number):
-            miu.append(medium_matrix[i * 3])
-            ebuxiro.append(medium_matrix[i * 3 + 1])
-            d_each_level.append(medium_matrix[1 * 3 + 2])
+            miu.append(float(medium_matrix[i * 3]))
+            ebuxiro.append(float(medium_matrix[i * 3 + 1]))
+            d_each_level.append(float(medium_matrix[1 * 3 + 2]))
 
         self.caculate()
 
     def caculate(self):
-        print(miu)
-        print(ebuxiro)
-        print(d_each_level)
+        global R_TE_current, R_TM_current, T_TE_current, T_TM_current, Z_n1
+        f = frequency
+        pi = 3.1416
+        theta1 = np.arange(1, 90)
+        theta = theta1 / 360 * 2 * pi
+        w = f / (2 * pi)
+        k_x = np.sqrt(w**2 * miu[0] * ebuxiro[0]) * np.sin(theta)
+        k_iz = []
+        for i in range(0, level_number):
+            k_i = np.sqrt(w**2 * miu[i] * ebuxiro[i])
+            k_iz.append(np.sqrt(k_i**2 - k_x**2))
+
+        R_TE = []
+        T_TE = []
+        R_TM = []
+        T_TM = []
+
+        for i in range(0, level_number - 1):
+
+            R_TE.append((miu[i + 1] * k_iz[i] - miu[i] * k_iz[i + 1]) / (miu[i + 1] * k_iz[i] + miu[i] * k_iz[i + 1]))
+            T_TE.append((2 * miu[i + 1] * k_iz[i]) / (miu[i + 1] * k_iz[i] + miu[i] * k_iz[i + 1]))
+            R_TM.append((ebuxiro[i + 1] * k_iz[i] - ebuxiro[i] * k_iz[i + 1]) / (ebuxiro[i + 1] * k_iz[i] + ebuxiro[i] * k_iz[i + 1]))
+            T_TM.append((2 * ebuxiro[i + 1] * k_iz[i]) / (ebuxiro[i + 1] * k_iz[i] + ebuxiro[i] * k_iz[i + 1]))
+            if i == 0:
+                R_TE_current = R_TE[0]
+                R_TM_current = R_TM[0]
+                T_TE_current = T_TE[0]
+                T_TM_current = T_TM[0]
+                continue
+            R_TE_current = (R_TE[i] + R_TE_current * np.exp(2 * complex(0, 1) * k_iz[i + 1] * (d_each_level[i] - d_each_level[i - 1]))) / (1 + R_TE[i] * R_TE_current * np.exp(2 * complex(0, 1) * k_iz[i + 1] * (d_each_level[i] - d_each_level[i - 1])))
+            R_TM_current = (R_TM[i] + R_TM_current * np.exp(2 * complex(0, 1) * k_iz[i + 1] * (d_each_level[i] - d_each_level[i - 1]))) / (1 + R_TM[i] * R_TM_current * np.exp(2 * complex(0, 1) * k_iz[i + 1] * (d_each_level[i] - d_each_level[i - 1])))
+
+        T_TM_current = R_TM_current + 1
+        T_TE_current = R_TE_current + 1
+
+        R_zn = R_TM_current[88]
+        Z_n1 = np.sqrt(miu[0] / ebuxiro[0]) * (1 + R_zn * np.exp(k_iz[0] * (-1) * complex(0, 1)))/ (1 - R_zn * np.exp(k_iz[0] * (-1) * complex(0, 1)))
+        Z_ni = np.sqrt(Z_n1[0].real**2 + Z_n1[0].imag**2)
+        print(Z_ni)
+        self.ui.surface_impedance_2.setText(str(Z_ni))
+        self.ui.surface_impedance_2.repaint()
+
+        print("miu = ", miu)
+        print("ebuxiro = ", ebuxiro)
+        print("d each level = ", d_each_level)
 
     def set_module_TE(self):
         global wave_Mode
@@ -119,29 +155,21 @@ class AppWindow(QMainWindow):
         self.figure_plot()
 
     def figure_plot(self):
-        global wave_Mode, curve_Mode
+        global wave_Mode, curve_Mode, R_TM, R_TE, T_TM, T_TE, R_TE_current
         figure_final = []
         if wave_Mode == 0 or curve_Mode == 0:
             return
         if wave_Mode == 1:
             if curve_Mode == 1:
-                x = [1, 2, 3, 4, 5]
-                figure_final = R_TE
-                figure_final = x
+                figure_final = R_TE_current
             elif curve_Mode == 2:
-                t = [5, 4, 3, 2, 1]
-                figure_final = T_TE
-                figure_final = t
+                figure_final = T_TE_current
 
         elif wave_Mode == 2:
             if curve_Mode == 1:
-                w = [6, 7, 8, 9, 10]
-                figure_final = R_TM
-                figure_final = w
+                figure_final = R_TM_current
             elif curve_Mode == 2:
-                r = [10, 9, 8, 7, 6]
-                # figure_final = T_TM
-                figure_final = r
+                figure_final = T_TM_current
         print('current config', curve_Mode, wave_Mode)
         self.ax.clear()
         self.ax.plot(figure_final)
